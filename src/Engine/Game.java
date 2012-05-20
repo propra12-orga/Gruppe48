2fd2b599296acc55bdf008756667c2ba6257f4da
Leonid Panich <panochk@gmail.com>
2012-05-20 16:11:07 +0400

    Start, Restart implementiert

--- src/Engine/Game.java
+++ src/Engine/Game.java
@@ -1,81 +1,90 @@
 package Engine;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.swing.*;
+
 import Field.Field;
 import Field.FieldGenerator;
 import GUI.GUI;
-import Objects.Bomb;
-import Objects.Player;
-
-public class Game implements Runnable, KeyListener {
+import Objects.*;
+public class Game implements Runnable {
 
 	private Field gameField;
-	private GameStates gameState;
-	private final FieldGenerator testGenerator;
-	private final Field testfield;
-	private final GUI gui;
-	char key;
+	public GameStates gameState;
+
+	private GUI gui;
+	public char key;
 	long gameSpeed;
 	Player player;
 	List<Bomb> bombList;
 	List<long[]> explosionList;
+	Calendar calendar;
 	long time;
-
-	public Game() {
-		explosionList = new ArrayList<long[]>();
-		time = Calendar.getInstance().getTimeInMillis();
-		bombList = new ArrayList<Bomb>();
-		gameSpeed = 100;
-		player = new Player(1, 1);
-		testGenerator = new FieldGenerator();
-		testfield = new Field();
-		testGenerator.setRandomAmount(5);
-		testGenerator.setRandomChance(50);
-		testGenerator.setModus(0);
-		testfield.insertMap(testGenerator.createSquareMap(15));
-		testfield.setPlayer(player);
-		gui = new GUI(testfield);
-		gui.setVisible(true);
-		gameState = GameStates.STARTED;
-		gui.addKeyListener(this);
-		run();
+	private Field testfield;
+	
+	public Game(Field field, Player player) {
+		restart(field, player, false);
+		//gui.addKeyListener(this);
+//		System.out.println(gui.isFocusable());
+//		run();
 	}
 
 	public Field getField() {
 		return gameField;
 	}
+	
+	public void insertGUI(GUI gui) {
+		this.gui = gui;
+	}
+	
+	public void restart(Field field, Player player, Boolean restart) {
+		this.player = player;
+		explosionList = new ArrayList<long[]>();
+		time = Calendar.getInstance().getTimeInMillis();
+		bombList = new ArrayList<Bomb>();
+		gameSpeed = 100;
+		gameState = GameStates.INITIALIZED;
+		testfield = field;
+		if (restart)
+			gameState = GameStates.STARTED;
+	}
 
 	@Override
 	public void run() {
-		while (true) {
+		Boolean doRestart = false;
+		while (true && !doRestart) {
 			switch (gameState) {
-			case STARTED:
-				start();
-				System.out.println("Game started");
-				break;
-			case PAUSED:
-				System.out.println("Game paused");
-				pauseGame();
-				break;
-			case VICTORY:
-				System.out.println("VICTORY");
-				restart();
-				break;
-			case GAMEOVER:
-				System.out.println("GAMEOVER");
-				restart();
-				break;
-			case INITIALIZED:
-				System.out.println("INITIALIZED");
-				break;
-			default:
-				System.out.println("default");
+				case STARTED:
+					start();
+					System.out.println("Game started");
+					break;
+				case VICTORY:
+					System.out.println("VICTORY");
+					doRestart = true;
+					break;
+				case GAMEOVER:
+					System.out.println("GAMEOVER");
+					doRestart = true;
+					break;
+				case INITIALIZED:
+					System.out.println("INITIALIZED");
+					break;
+				default:
+					System.out.println("default");
+					break;
 			}
+		}
+		if (doRestart) {
+			restart(Game.createNewField(), new Player(1,1), true);
+			run();
 		}
 	}
 
@@ -84,277 +93,197 @@ public class Game implements Runnable, KeyListener {
 	}
 
 	public void start() {
-		while (gameState == GameStates.STARTED) {
-			time = Calendar.getInstance().getTimeInMillis();
-			if (gui.isVisible() == false) {
-				System.exit(0);
-			}
-			handleBombs();
-			handleMovement();
-			gui.insertField(testfield);
-			gui.repaint();
-			try {
-				Thread.sleep(gameSpeed);
-			} catch (Exception ex) {
-				System.out.println(ex);
-			}
-		}
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		key = e.getKeyChar();
-
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-	}
-
-	private void handleBombs() {
 		ArrayList<int[]> exList;
-		for (int i = 0; i < bombList.size(); i++) {
-			if (bombList.get(i).getTimer() <= time) {
-				exList = new ArrayList<int[]>();
-				exList.add(new int[2]);
-				exList.get(exList.size() - 1)[0] = bombList.get(i)
-						.getPosition()[1];
-				exList.get(exList.size() - 1)[1] = bombList.get(i)
-						.getPosition()[0];
-				for (int j = 1; j < bombList.get(i).getRadius(); j++) {
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] - j).getPlayer() != null) {
-						gameState = GameStates.GAMEOVER;
-					}
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] - j).getBomb() != null) {
-						int tmp[] = new int[2];
-						tmp[0] = bombList.get(i).getPosition()[0] - j;
-						tmp[1] = bombList.get(i).getPosition()[1];
-						for (int k = 0; k < bombList.size(); k++) {
-							if ((bombList.get(k).getPosition()[0] == tmp[0])
-									&& (bombList.get(k).getPosition()[1] == tmp[1])) {
-								bombList.get(k).detonate();
-								i = 0;
-							}
+		while(gameState==GameStates.STARTED)
+		{		
+			time = Calendar.getInstance().getTimeInMillis();
+			for(int i = 0; i < bombList.size(); i++)
+			{
+				if(bombList.get(i).getTimer() <= time)
+				{
+					exList = new ArrayList<int[]>();
+					exList.add(new int[2]);
+					exList.get(exList.size()-1)[0] = bombList.get(i).getPosition()[1];
+					exList.get(exList.size()-1)[1] = bombList.get(i).getPosition()[0];
+					for(int j = 1; j < bombList.get(i).getRadius(); j++)
+					{					
+						if(testfield.getField(bombList.get(i).getPosition()[1], bombList.get(i).getPosition()[0] - j).getPlayer() != null)
+						{
+							gameState = GameStates.GAMEOVER;
+						}
+						if(testfield.getField(bombList.get(i).getPosition()[1], bombList.get(i).getPosition()[0] - j).getContent() == 1)
+						{
+							exList.add(new int[2]);
+							exList.get(exList.size()-1)[0] = bombList.get(i).getPosition()[1];
+							exList.get(exList.size()-1)[1] = bombList.get(i).getPosition()[0] - j;							
+						}
+						else
+						{
+							break;							
 						}
 					}
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] - j).getContent() == 1) {
-						exList.add(new int[2]);
-						exList.get(exList.size() - 1)[0] = bombList.get(i)
-								.getPosition()[1];
-						exList.get(exList.size() - 1)[1] = bombList.get(i)
-								.getPosition()[0] - j;
-					} else {
-						break;
-					}
-				}
-				for (int j = 1; j < bombList.get(i).getRadius(); j++) {
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] + j).getPlayer() != null) {
-						gameState = GameStates.GAMEOVER;
-					}
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] + j).getBomb() != null) {
-						int tmp[] = new int[2];
-						tmp[0] = bombList.get(i).getPosition()[0] + j;
-						tmp[1] = bombList.get(i).getPosition()[1];
-						for (int k = 0; k < bombList.size(); k++) {
-							if ((bombList.get(k).getPosition()[0] == tmp[0])
-									&& (bombList.get(k).getPosition()[1] == tmp[1])) {
-								bombList.get(k).detonate();
-								i = 0;
-							}
+					for(int j = 1; j < bombList.get(i).getRadius(); j++)
+					{
+						if(testfield.getField(bombList.get(i).getPosition()[1], bombList.get(i).getPosition()[0] + j).getPlayer() != null)
+						{
+							gameState = GameStates.GAMEOVER;
+						}
+						
+						if(testfield.getField(bombList.get(i).getPosition()[1], bombList.get(i).getPosition()[0] + j).getContent() == 1)
+						{
+							exList.add(new int[2]);
+							exList.get(exList.size()-1)[0] = bombList.get(i).getPosition()[1];
+							exList.get(exList.size()-1)[1] = bombList.get(i).getPosition()[0] + j;			
+						}
+						else
+						{
+							break;							
 						}
 					}
-					if (testfield.getField(bombList.get(i).getPosition()[1],
-							bombList.get(i).getPosition()[0] + j).getContent() == 1) {
-						exList.add(new int[2]);
-						exList.get(exList.size() - 1)[0] = bombList.get(i)
-								.getPosition()[1];
-						exList.get(exList.size() - 1)[1] = bombList.get(i)
-								.getPosition()[0] + j;
-					} else {
-						break;
-					}
-				}
-				for (int j = 1; j < bombList.get(i).getRadius(); j++) {
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] - j,
-							bombList.get(i).getPosition()[0]).getPlayer() != null) {
-						gameState = GameStates.GAMEOVER;
-					}
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] - j,
-							bombList.get(i).getPosition()[0]).getBomb() != null) {
-						int tmp[] = new int[2];
-						tmp[0] = bombList.get(i).getPosition()[0];
-						tmp[1] = bombList.get(i).getPosition()[1] - j;
-						for (int k = 0; k < bombList.size(); k++) {
-							if ((bombList.get(k).getPosition()[0] == tmp[0])
-									&& (bombList.get(k).getPosition()[1] == tmp[1])) {
-								bombList.get(k).detonate();
-								i = 0;
-							}
+					for(int j = 1; j < bombList.get(i).getRadius(); j++)
+					{
+						if(testfield.getField(bombList.get(i).getPosition()[1] - j, bombList.get(i).getPosition()[0]).getPlayer() != null)
+						{
+							gameState = GameStates.GAMEOVER;
+						}
+						
+						if(testfield.getField(bombList.get(i).getPosition()[1] - j, bombList.get(i).getPosition()[0]).getContent() == 1)
+						{
+							exList.add(new int[2]);
+							exList.get(exList.size()-1)[0] = bombList.get(i).getPosition()[1] - j;
+							exList.get(exList.size()-1)[1] = bombList.get(i).getPosition()[0];			
+						}
+						else
+						{
+							break;							
 						}
 					}
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] - j,
-							bombList.get(i).getPosition()[0]).getContent() == 1) {
-						exList.add(new int[2]);
-						exList.get(exList.size() - 1)[0] = bombList.get(i)
-								.getPosition()[1] - j;
-						exList.get(exList.size() - 1)[1] = bombList.get(i)
-								.getPosition()[0];
-					} else {
-						break;
-					}
-				}
-				for (int j = 1; j < bombList.get(i).getRadius(); j++) {
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] + j,
-							bombList.get(i).getPosition()[0]).getPlayer() != null) {
-						gameState = GameStates.GAMEOVER;
-					}
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] + j,
-							bombList.get(i).getPosition()[0]).getBomb() != null) {
-						int tmp[] = new int[2];
-						tmp[0] = bombList.get(i).getPosition()[0];
-						tmp[1] = bombList.get(i).getPosition()[1] + j;
-						for (int k = 0; k < bombList.size(); k++) {
-							if ((bombList.get(k).getPosition()[0] == tmp[0])
-									&& (bombList.get(k).getPosition()[1] == tmp[1])) {
-								bombList.get(k).detonate();
-								i = 0;
-							}
+					for(int j = 1; j < bombList.get(i).getRadius(); j++)
+					{
+						if(testfield.getField(bombList.get(i).getPosition()[1] + j, bombList.get(i).getPosition()[0]).getPlayer() != null)
+						{
+							gameState = GameStates.GAMEOVER;
 						}
-					}
-					if (testfield.getField(
-							bombList.get(i).getPosition()[1] + j,
-							bombList.get(i).getPosition()[0]).getContent() == 1) {
-						exList.add(new int[2]);
-						exList.get(exList.size() - 1)[0] = bombList.get(i)
-								.getPosition()[1] + j;
-						exList.get(exList.size() - 1)[1] = bombList.get(i)
-								.getPosition()[0];
-					} else {
-						break;
-					}
+						
+						if(testfield.getField(bombList.get(i).getPosition()[1] + j, bombList.get(i).getPosition()[0]).getContent() == 1)
+						{
+							exList.add(new int[2]);
+							exList.get(exList.size()-1)[0] = bombList.get(i).getPosition()[1] + j;
+							exList.get(exList.size()-1)[1] = bombList.get(i).getPosition()[0];			
+						}
+						else
+						{
+							break;							
+						}
+					}															
+				
+					explosionList.add(new long[1]);
+					explosionList.get(0)[0] = calendar.getInstance().getTimeInMillis() + 500;
+					gui.panel.addExplosions(exList);
+					exList = null;
+					testfield.removeBomb(bombList.get(i));
+					bombList.remove(i);
 				}
-
-				explosionList.add(new long[1]);
-				explosionList.get(0)[0] = Calendar.getInstance()
-						.getTimeInMillis() + 500;
-				gui.panel.addExplosions(exList);
-				exList = null;
-				testfield.removeBomb(bombList.get(i));
-				bombList.remove(i);
+					
 			}
-		}
-		for (int i = 0; i < explosionList.size(); i++) {
-			if (explosionList.get(i)[0] < (Calendar.getInstance()
-					.getTimeInMillis())) {
-				gui.panel.removeExplosions();
-				explosionList.remove(0);
-			}
-		}
-	}
-
-	private void handleMovement() {
-		switch (key) {
-		case 'w':
-			switch (testfield.getField(player.getPosition()[1],
-					player.getPosition()[0] - 1).getContent()) {
-			case 1:
-				if (testfield.getField(player.getPosition()[1],
-						player.getPosition()[0] - 1).getBomb() != null) {
-					break;
+			
+			for(int i = 0; i < explosionList.size(); i++)
+			{			
+				if(explosionList.get(i)[0] <  (calendar.getInstance().getTimeInMillis()))
+				{
+					gui.panel.removeExplosions();
+					explosionList.remove(0);
 				}
+			}
+			switch(key)
+			{
+			case 'w':
+				switch(testfield.getField(player.getPosition()[1], player.getPosition()[0] - 1).getContent())
+				{
+				case 1:
 				testfield.removePlayer(player);
 				player.moveUp();
 				testfield.setPlayer(player);
 				break;
-			case 3:
+				case 3:
 				gameState = GameStates.VICTORY;
-			}
-			break;
-		case 'a':
-			switch (testfield.getField(player.getPosition()[1] - 1,
-					player.getPosition()[0]).getContent()) {
-			case 1:
-				if (testfield.getField(player.getPosition()[1] - 1,
-						player.getPosition()[0]).getBomb() != null) {
-					break;
 				}
+				break;
+			case 'a':
+				switch(testfield.getField(player.getPosition()[1] - 1, player.getPosition()[0]).getContent())
+				{
+				case 1:
 				testfield.removePlayer(player);
 				player.moveLeft();
 				testfield.setPlayer(player);
 				break;
-			case 3:
+				case 3:
 				gameState = GameStates.VICTORY;
-			}
-			break;
-		case 's':
-			switch (testfield.getField(player.getPosition()[1],
-					player.getPosition()[0] + 1).getContent()) {
-			case 1:
-				if (testfield.getField(player.getPosition()[1],
-						player.getPosition()[0] + 1).getBomb() != null) {
-					break;
 				}
+				break;
+			case 's':		
+				switch(testfield.getField(player.getPosition()[1], player.getPosition()[0] + 1).getContent())
+				{
+				case 1:
 				testfield.removePlayer(player);
 				player.moveDown();
 				testfield.setPlayer(player);
 				break;
-			case 3:
+				case 3:
 				gameState = GameStates.VICTORY;
-			}
-			break;
-		case 'd':
-			switch (testfield.getField(player.getPosition()[1] + 1,
-					player.getPosition()[0]).getContent()) {
-			case 1:
-				if (testfield.getField(player.getPosition()[1] + 1,
-						player.getPosition()[0]).getBomb() != null) {
-					break;
 				}
+				break;
+			case 'd':
+				switch(testfield.getField(player.getPosition()[1] + 1, player.getPosition()[0]).getContent())
+				{
+				case 1:
 				testfield.removePlayer(player);
 				player.moveRight();
 				testfield.setPlayer(player);
 				break;
-			case 3:
+				case 3:
 				gameState = GameStates.VICTORY;
+				}
+				break;
+			case KeyEvent.VK_SPACE:
+				bombList.add(new Bomb(player.getPosition()[0], player.getPosition()[1], time));
+				testfield.setBomb(bombList.get(bombList.size()-1));
+				break;		
 			}
-			break;
-		case KeyEvent.VK_SPACE:
-			bombList.add(new Bomb(player.getPosition()[0],
-					player.getPosition()[1], time));
-			testfield.setBomb(bombList.get(bombList.size() - 1));
-			break;
+			key = 0;
+			gui.insertField(testfield);
+			gui.repaint();
+			try{
+			Thread.sleep(gameSpeed);
+			}catch(Exception ex)
+			{
+				System.out.println(ex);
+			}
 		}
-		key = 0;
 	}
-
-	public void restart() {
-		for (int i = 0; i <= bombList.size(); i++) {
-			gui.panel.removeExplosions();
-		}
-		bombList.clear();
-		player.setPosition(1, 1);
+	
+	public static Field createNewField() {
+		FieldGenerator testGenerator = new FieldGenerator();
+		Field testfield = new Field();
+		testGenerator.setRandomAmount(5);
+		testGenerator.setRandomChance(50);
+		testGenerator.setModus(0);
 		testfield.insertMap(testGenerator.createSquareMap(15));
+		Player player = new Player(1,1);
 		testfield.setPlayer(player);
-		gui.insertField(testfield);
-		gui.repaint();
-		gameState = GameStates.STARTED;
+		return testfield;
 	}
-
-	public static void main(String args[]) {
-		new Game();
+	
+	public static void main(String args[])
+	{
+		Field field = createNewField();
+		Game game = new Game(field, new Player(1,1));
+		GUI gui = new GUI (field, game);
+		gui.setVisible(true);
+		game.insertGUI(gui);
+		
+		Thread gameThread = new Thread(game);
+		gameThread.start();
+		
 	}
 }
