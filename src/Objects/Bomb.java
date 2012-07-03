2bccc54b7f5819aae24fd1e9bf33a930abc26794
stegic <cst@cst-PC.(none)>
2012-07-03 16:44:08 +0200

    Kommentare hinzugefuegt

--- src/Objects/Bomb.java
+++ src/Objects/Bomb.java
@@ -19,17 +19,19 @@ public class Bomb implements Serializable {
 	 * Ist true, falls Bombe explodiert, ist sonst false
 	 */
 	public static boolean isExploded = false;
-	public static boolean bombStatus = false;
-	public static boolean bombStatusP2 = false;
-	long delay = 7 * 1000;
+	public static boolean bombStatus = false; // wenn false darf Player1 neue
+												// bomben legen
+	public static boolean bombStatusP2 = false; // //wenn false darf player2
+												// neue bomben legen
+	long delay = 3 * 1000;
 	long explosionTime;
 	int x;
 	int y;
 	int radius;
-	static int bombMax;
-	static int bombMaxP2;
-	public static int currentPlaced = 0;
-	public static int currentPlacedP2 = 0;
+	static int bombMax;// Anzahl maximal erlaubter Bomben Player1
+	static int bombMaxP2;// Anzahl maximal erlaubter Bomben Player2
+	public static int currentPlaced;// Anzahl aktuell gelegter Bomben Player1
+	public static int currentPlacedP2;// Anzahl aktuell gelegter Bomben Player2
 
 	/**
 	 * Konstruktor der Klasse Bomb
@@ -112,7 +114,7 @@ public class Bomb implements Serializable {
 	/**
 	 * Vergleicht den Wert aktuell gelegter Bomben(Player2) mit dem Wert maximal
 	 * erlaubter legbarer Bomben. Ist der Wert kleiner, bekommt bombstatus den
-	 * wert false Ist er größer dann true
+	 * wert false Ist er größer dann true.
 	 */
 	public static void setBombStatusP2() {
 		if (getCurrentPlacedP2() <= bombMaxP2) {
@@ -129,52 +131,118 @@ public class Bomb implements Serializable {
 		}
 	}
 
+	/**
+	 * wird zum Abfragen des aktuellen Bombenstatus verwendet
+	 * 
+	 * @return bombStatus gibt den Bombenstatus für Player1 zurueck, um zu
+	 *         pruefen, ob weiter Bomben gelegt werden duerfen
+	 */
 	public static boolean getBombStatus() {
 		return bombStatus;
 	}
+
+	/**
+	 * wird zum Abfragen des aktuellen Bombenstatus verwendet
+	 * 
+	 * @return bombStatus gibt den Bombenstatus für Player2 zurueck, um zu
+	 *         pruefen, ob weiter Bomben gelegt werden duerfen
+	 */
 
 	public static boolean getBombStatusP2() {
 		return bombStatusP2;
 	}
 
+	/**
+	 * setzt den Wert von currentPlaced fuer Player1 um eins hoeher, somit
+	 * koennen die Bomben gezaehlt werden
+	 */
 	public static void setCurrentPlacedBomb() {
 		currentPlaced += 1;
 	}
 
+	/**
+	 * wird zum Abfragen der bereits gesetzten Bomben von Player1 verwendet
+	 * 
+	 * @return currentPlaced gibt die Anzahl bereits gesetzter Bomben zurück
+	 */
 	public static int getCurrentPlaced() {
 		return currentPlaced;
 	}
+
+	/**
+	 * setzt den Wert von currentPlaced fuer Player2 um eins hoeher, somit
+	 * koennen die Bomben gezaehlt werden
+	 */
 
 	public static void setCurrentPlacedBombP2() {
 		currentPlacedP2 += 1;
 	}
 
+	/**
+	 * wird zum Abfragen der bereits gesetzten Bomben von Player2 verwendet
+	 * 
+	 * @return currentPlacedP2 gibt die Anzahl bereits gesetzter Bomben zurück
+	 */
 	public static int getCurrentPlacedP2() {
 		return currentPlacedP2;
 	}
 
+	/**
+	 * setzt den Wert der maximal legbaren Bomben fuer Player1 um eins hoeher.
+	 * dies geschieht, wenn ein BombenItem eingesammelt wird
+	 */
 	public static void setBombMax() {
 		bombMax += 1;
 	}
 
+	/**
+	 * wird fuer Player1 abgefragt, damit die Zahl aktuell gelegter Bomben mit
+	 * der Zahl der maximal erlaubten Bomben verglichen werden kann
+	 * 
+	 * @return bombMax gibt den Wert zurück der maximal erlaubten bomben
+	 */
 	public static int getBombMax() {
 		return bombMax;
 	}
 
+	/**
+	 * setzt den Wert der maximal legbaren Bomben fuer Player2 um eins hoeher.
+	 * dies geschieht, wenn ein BombenItem eingesammelt wird
+	 */
 	public static void setBombMaxP2() {
 		bombMaxP2 += 1;
 	}
 
+	/**
+	 * wird fuer Player2 abgefragt, damit die Zahl aktuell gelegter Bomben mit
+	 * der Zahl der maximal erlaubten Bomben verglichen werden kann
+	 * 
+	 * @return bombMax gibt den Wert zurück der maximal erlaubten bomben
+	 */
 	public static int getBombMaxP2() {
 		return bombMaxP2;
 	}
 
+	/**
+	 * beim Start des Spieles soll der Wert fuer Player1 der maximal erlaubten
+	 * Bomben wieder 1 sein
+	 * 
+	 * @param newBombMax
+	 *            legt den wert von bombMax am start des Spieles fest
+	 */
 	public static void setBombMaxOnStart(int newBombMax) {
 		bombMax = newBombMax;
 	}
 
+	/**
+	 * beim Start des Spieles soll der Wert fuer Player2 der maximal erlaubten
+	 * Bomben wieder 1 sein
+	 * 
+	 * @param newBombMax
+	 *            legt den wert von bombMax am start des Spieles fest
+	 */
 	public static void setBombMaxOnStartP2(int newBombMax) {
-		bombMax = newBombMax;
+		bombMaxP2 = newBombMax;
 	}
 
 }
